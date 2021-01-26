d2cb1ebe0c53bdb678853a814dd5efc55a02e0dc
Matt George <mbgeorge48@gmail.com>
2021-01-26 23:41:03 +0000

    Commiting the working copy function, may add logging

--- picture_backup.py
+++ picture_backup.py
@@ -4,20 +4,6 @@ import socket
 import hashlib
 import sys
 
-# Functions
-# Copy from host to backup device
-# Copy from backup device to host
-# Don't copy files that are already there
-
-
-# Needs rewriting so we can deduplicate
-# def backup():
-#     for item in os.listdir(SOURCE_FOLDER):
-#         if item[0] != '.' and item[-3:] != '.py':
-#             print(f'Backing up {item}')
-#             shutil.copytree(os.path.join(SOURCE_FOLDER, item),
-#                             os.path.join(DESTINATION_FOLDER, item))
-
 WINDOWS_HOSTNAME = 'griffin'
 MAC_HOSTNAME = 'phoenix'
 
@@ -26,6 +12,7 @@ class PictureBackupinator:
     destination_folder = os.sep
     skip_duplicates = True
     seen_file_hashes = list()
+    files_to_backup = list()
 
     def __init__(self):
         temp = self.get_parameters('host')
@@ -48,21 +35,19 @@ class PictureBackupinator:
             folder_b = os.path.join(os.sep, 'Volumes', 'camera_roll')
         elif hostname == WINDOWS_HOSTNAME:
             self.OS = 'Windows'
-            folder_a = 'x'
+            folder_a = os.path.join(os.sep, 'source')
             folder_b = os.path.join(os.sep, 'camera_roll')
         print(f'Running backup script using {self.OS} settings\nHostname: {hostname}')
         print(f'Function: {self.program_function}\nSkip duplicates is set to {str(self.skip_duplicates)}')
         print('='*60)
 
         self.sort_function(folder_a, folder_b)
-
         self.check_destination_folder()
 
         if self.skip_duplicates:
-            self.scan_destination()
-
-        
-
+            self.scan_directories(self.destination_folder)
+        self.scan_directories(self.source_folder)
+        self.copy_files()
 
     def get_parameters(self, value):
         return_value = True
@@ -87,10 +72,15 @@ class PictureBackupinator:
             print(f'Something has gone wrong when trying to make this folder:\n{self.destination_folder}\n{exc}')
             sys.exit()
 
-    def scan_destination(self):
-        for root, dir, files in os.walk(self.destination_folder):
+    def scan_directories(self, directory):
+        for root, dir, files in os.walk(directory):
             for file in files:
-                self.seen_file_hashes = self.get_hash(os.path.join(root,file))
+                file_path = os.path.join(root,file)
+                if directory  == self.destination_folder:
+                    self.seen_file_hashes.append(self.get_hash(file_path))
+                elif directory == self.source_folder:
+                    if not self.get_hash(file_path) in self.seen_file_hashes:
+                        self.files_to_backup.append(file_path)
 
     def get_hash(self, file_path):
         blocksize = 65536
@@ -108,7 +98,11 @@ class PictureBackupinator:
                 f'Permission denied trying to read "{file_path}" marked it as a duplicate')
             return 1
 
-    
+    def copy_files(self):
+        for file_path in self.files_to_backup:
+            dest_file_path = file_path.replace(self.source_folder, self.destination_folder)
+            os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+            shutil.copy2(file_path, dest_file_path)
 
 if __name__ == "__main__":
     backup_pictures = PictureBackupinator()
