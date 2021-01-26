a41ffb5453e93aa2d5b18ffa329c444f804429b9
Matt George <mbgeorge48@gmail.com>
2021-01-26 23:13:47 +0000

    Scanning the destination folder to get all the file hashes

--- picture_backup.py
+++ picture_backup.py
@@ -24,9 +24,22 @@ MAC_HOSTNAME = 'phoenix'
 class PictureBackupinator:
     source_folder = os.sep
     destination_folder = os.sep
+    skip_duplicates = True
+    seen_file_hashes = list()
 
     def __init__(self):
-        self.program_function = self.get_parameters()
+        temp = self.get_parameters('host')
+        if temp:
+            self.program_function = 'Host to backup device'
+        else: 
+            self.program_function = 'Backup device to host'
+
+        temp = self.get_parameters('duplicate')
+        if temp:
+            self.skip_duplicates = True
+        else: 
+            self.skip_duplicates = False
+
         hostname = socket.gethostname()
         print('='*60)
         if hostname == MAC_HOSTNAME:
@@ -36,22 +49,26 @@ class PictureBackupinator:
         elif hostname == WINDOWS_HOSTNAME:
             self.OS = 'Windows'
             folder_a = 'x'
-            folder_b = os.path.join(os.sep, 'd', 'camera_roll')
+            folder_b = os.path.join(os.sep, 'camera_roll')
         print(f'Running backup script using {self.OS} settings\nHostname: {hostname}')
-        print(f'Function: {self.program_function}')
+        print(f'Function: {self.program_function}\nSkip duplicates is set to {str(self.skip_duplicates)}')
         print('='*60)
 
         self.sort_function(folder_a, folder_b)
-        # self.check_destination_folder()
 
-    def get_parameters(self):
-        try:
-            if sys.argv[1] == "Host":
-                return "Backup device to host"
-            else:
-                raise IndexError
-        except IndexError:
-            return "Host to backup device"
+        self.check_destination_folder()
+
+        if self.skip_duplicates:
+            self.scan_destination()
+
+        
+
+
+    def get_parameters(self, value):
+        return_value = True
+        if value in sys.argv:
+            return_value = False
+        return return_value
 
     def sort_function(self, folder_a, folder_b):
         if self.program_function == "Host to backup device":
@@ -64,12 +81,34 @@ class PictureBackupinator:
 
     def check_destination_folder(self):
         try:
-            if not os.path.exists(DESTINATION_FOLDER):
-                os.makedirs(DESTINATION_FOLDER, exist_ok=True)
+            if not os.path.exists(self.destination_folder):
+                os.makedirs(self.destination_folder, exist_ok=True)
         except Exception as exc:
-            print(f'Something has gone wrong when trying to make this folder:\n{DESTINATION_FOLDER}\n{exc}')
+            print(f'Something has gone wrong when trying to make this folder:\n{self.destination_folder}\n{exc}')
             sys.exit()
 
+    def scan_destination(self):
+        for root, dir, files in os.walk(self.destination_folder):
+            for file in files:
+                self.seen_file_hashes = self.get_hash(os.path.join(root,file))
+
+    def get_hash(self, file_path):
+        blocksize = 65536
+        try:
+            this_file = open(file_path, 'rb')
+            hasher = hashlib.md5()
+            buf = this_file.read(blocksize)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = this_file.read(blocksize)
+            this_file.close()
+            return hasher.hexdigest()
+        except PermissionError:
+            self.file_info.warning(
+                f'Permission denied trying to read "{file_path}" marked it as a duplicate')
+            return 1
+
+    
 
 if __name__ == "__main__":
     backup_pictures = PictureBackupinator()
